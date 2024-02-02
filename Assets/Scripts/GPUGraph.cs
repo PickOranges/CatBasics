d130907a29229536508898f41162f9295a14af8a
chengzimdl@gmail.com <chengzimdl@gmail.com>
2024-02-02 14:45:05 +0100

    update

--- Assets/Scripts/GPUGraph.cs
+++ Assets/Scripts/GPUGraph.cs
@@ -10,15 +10,57 @@ public class GraphGPU : MonoBehaviour   // C# script to manage a compute shader
     [SerializeField]
     FunctionLibrary.FunctonName function;
 
+    ComputeBuffer positionsBuffer;
+
+    [SerializeField]
+    ComputeShader computeShader;
+
+    [SerializeField]
+    Material material;
+
+    [SerializeField]
+    Mesh mesh;
+
+    static readonly int
+    positionsId = Shader.PropertyToID("_Positions"),
+    resolutionId = Shader.PropertyToID("_Resolution"),
+    stepId = Shader.PropertyToID("_Step"),
+    timeId = Shader.PropertyToID("_Time");
+    void UpdateFunctionOnGPU()
+    {
+        float step = 2f / resolution;
+        computeShader.SetInt(resolutionId, resolution);
+        computeShader.SetFloat(stepId, step);
+        computeShader.SetFloat(timeId, Time.time);
+
+        computeShader.SetBuffer(0, positionsId, positionsBuffer);
+
+        int groups = Mathf.CeilToInt(resolution / 8f);
+        computeShader.Dispatch(0, groups, groups, 1);
+    }
+
+    //private void Awake()
+    //{
+    //    positionsBuffer = new ComputeBuffer(resolution * resolution, 3 * 4);  // 3*4 ? Without any alignment to 16 ?
+    //}
+
+    void OnEnable()
+    {
+        positionsBuffer = new ComputeBuffer(resolution * resolution, 3 * 4);
+    }
+
+    void OnDisable()
+    {
+        positionsBuffer.Release();
+        positionsBuffer = null;
+    }
+
 
     private void Update()
     {
-        Function f = GetFunction(function); 
-        float time = Time.time;
-        float step = 2f / resolution;
-        float v = 0.5f * step - 1f;  
-
-
+        UpdateFunctionOnGPU();
+        var bounds = new Bounds(Vector3.zero, Vector3.one * (2f + 2f / resolution));
+        Graphics.DrawMeshInstancedProcedural(mesh,0,material,bounds,positionsBuffer.count);
     }
 
 }
