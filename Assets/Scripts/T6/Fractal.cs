337cd7f03faaeeb4379735cfbc854a0a937b6358
chengzimdl@gmail.com <chengzimdl@gmail.com>
2024-02-05 10:56:59 +0100

    Create 2 children spheres for each recursion.

--- Assets/Scripts/T6/Fractal.cs
+++ Assets/Scripts/T6/Fractal.cs
@@ -11,7 +11,19 @@ public class Fractal : MonoBehaviour
 
         if(depth<=1) return;    
 
+        // create Child 1
         Fractal child = Instantiate(this);
         child.depth = depth-1;
+        // assign the parent's TF to the child, and then change it to correct position.
+        child.transform.SetParent(transform, false);
+        child.transform.localPosition = 0.75f * Vector3.right; // decrease the distance and shrink the size of ball each recursion
+        child.transform.localScale = 0.5f * Vector3.one;
+
+        // create Child 2
+        child = Instantiate(this);
+        child.depth = depth - 1;
+        child.transform.SetParent(transform, false);
+        child.transform.localPosition = 0.75f * Vector3.up;
+        child.transform.localScale = 0.5f * Vector3.one;
     }
 }
