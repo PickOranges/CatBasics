1296702d3c09ccb86cebe8979de469554f2eddcf
chengzimdl@gmail.com <chengzimdl@gmail.com>
2024-02-05 17:14:37 +0100

    remove the recursion and switch to BFS, i.e. iteration / for-loop method.

--- Assets/Scripts/T6/Fractal.cs
+++ Assets/Scripts/T6/Fractal.cs
@@ -5,43 +5,102 @@ public class Fractal : MonoBehaviour
     [SerializeField, Range(1, 8)]
     int depth = 4;
 
-    private void Start()
+    [SerializeField]
+    Mesh mesh;
+
+    [SerializeField]
+    Material material;
+
+    FractalPart[][] parts;
+
+    static Vector3[] directions = {
+        Vector3.up, Vector3.right, Vector3.left, Vector3.forward, Vector3.back
+    };
+
+    static Quaternion[] rotations = {
+        Quaternion.identity,
+        Quaternion.Euler(0f, 0f, -90f), Quaternion.Euler(0f, 0f, 90f),
+        Quaternion.Euler(90f, 0f, 0f), Quaternion.Euler(-90f, 0f, 0f)
+    };
+
+    struct FractalPart
     {
-        name = depth.ToString();
+        public Vector3 direction;
+        public Quaternion rotation;
+        public Transform transform;
+    }
 
-        if(depth<=1) return;
+    FractalPart CreatePart(int levelIndex, int childIndex, float scale)
+    {
+        var go = new GameObject("Fractal Part L" + levelIndex + " C" + childIndex);
 
-        // S1. create children 
-        Fractal childA = CreateChild(Vector3.up, Quaternion.identity);
-        Fractal childB = CreateChild(Vector3.right, Quaternion.Euler(0f, 0f, -90f));
-        Fractal childC = CreateChild(Vector3.left, Quaternion.Euler(0f, 0f, 90f));
-        Fractal childD = CreateChild(Vector3.forward, Quaternion.Euler(90f, 0f, 0f));
-        Fractal childE = CreateChild(Vector3.back, Quaternion.Euler(-90f, 0f, 0f));
-        // S2. bind them to the parent
-        childA.transform.SetParent(transform, false);
-        childB.transform.SetParent(transform, false);
-        childC.transform.SetParent(transform, false);
-        childD.transform.SetParent(transform, false);
-        childE.transform.SetParent(transform, false);
-        // Note:
-        // We don't bind to parent immediately, because that will change the hiarchy of the parent,
-        // and by the time of creating the 2nd child, it will take both parent and its 1st child as the reference,
-        // thus the result is wrong.
-        // Solution: create all children, then bind them to parent one-by-one.
+        return new FractalPart
+        {
+            direction = directions[childIndex],
+            rotation = rotations[childIndex],
+            transform = go.transform
+        };
+    }
+
+    void Awake()
+    {
+        // S1. create a vector
+        parts = new FractalPart[depth][ ];
+        for (int i = 0, length = 1; i < parts.Length; i++, length *= 5)
+        {
+            parts[i] = new FractalPart[length];
+        }
+
+        // S2. then fill it
+        float scale = 1f;
+        parts[0][0]=CreatePart(0, 0, scale);       // root node is NOT a child of any node.                       
+        for (int li = 1; li < parts.Length; li++)  // level idx
+        {
+            scale *= 0.5f;
+            FractalPart[] levelParts = parts[li];   // take the init. memory blocks and fill it
+            for (int fpi = 0; fpi < levelParts.Length; fpi+=5)  // first place idx
+            {
+                for (int ci = 0; ci < 5; ci++)
+                {
+                    levelParts[fpi+ci]=CreatePart(li, ci, scale);
+                }
+            }
+            // Why we don't need this line?
+            // Because all C# vector/array are on the heap, thus this operation is the same as shallow copy in C++.
+            // Means that if you change the new ref' value, you also change the original data, because there was only one copy of data on heap.
+            //parts[li] = levelParts;  
+
+            Debug.Log("level size: " + parts[li].Length);
+        }
     }
 
     void Update()
     {
-        transform.Rotate(0f, 22.5f * Time.deltaTime, 0f);
-    }
+        Quaternion deltaRotation = Quaternion.Euler(0f, 22.5f * Time.deltaTime, 0f);
+        FractalPart rootPart = parts[0][0];
+        rootPart.rotation *= deltaRotation;
+        rootPart.transform.localRotation = rootPart.rotation;
+        parts[0][0] = rootPart;
 
-    Fractal CreateChild(Vector3 direction, Quaternion rotation)
-    {
-        Fractal child = Instantiate(this);
-        child.depth = depth - 1;
-        child.transform.localPosition = 0.75f * direction;
-        child.transform.localRotation = rotation;
-        child.transform.localScale = 0.5f * Vector3.one;
-        return child;
+        // Root node cannot move(actually it can rotate, but that will make no difference).
+        // Thus we don't have to update root node.
+        for (int li = 1; li < parts.Length; li++)
+        {
+            FractalPart[] levelParts = parts[li];
+            FractalPart[] parentParts = parts[li - 1];
+            for (int fpi = 0; fpi < levelParts.Length; fpi++)
+            {
+                Transform parentTransform = parentParts[fpi / 5].transform;  // This parent node idx calculation is the same as binary tree.
+                FractalPart part = levelParts[fpi];
+                part.rotation *= deltaRotation;
+                part.transform.localRotation =
+                    parentTransform.localRotation * part.rotation;
+
+                part.transform.localPosition = 
+                    parentTransform.localPosition +
+                    parentTransform.localRotation *
+                    (1.5f * part.transform.localScale.x * part.direction);
+            }
+        }
     }
 }
