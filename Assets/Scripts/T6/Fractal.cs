8c046778db178cfe4cdd35581d62b6bd2584a6aa
chengzimdl@gmail.com <chengzimdl@gmail.com>
2024-02-05 11:23:10 +0100

    Add all children.

--- Assets/Scripts/T6/Fractal.cs
+++ Assets/Scripts/T6/Fractal.cs
@@ -12,11 +12,17 @@ public class Fractal : MonoBehaviour
         if(depth<=1) return;
 
         // S1. create children 
-        Fractal childA = CreateChild(Vector3.up);
-        Fractal childB = CreateChild(Vector3.right);
+        Fractal childA = CreateChild(Vector3.up, Quaternion.identity);
+        Fractal childB = CreateChild(Vector3.right, Quaternion.Euler(0f, 0f, -90f));
+        Fractal childC = CreateChild(Vector3.left, Quaternion.Euler(0f, 0f, 90f));
+        Fractal childD = CreateChild(Vector3.forward, Quaternion.Euler(90f, 0f, 0f));
+        Fractal childE = CreateChild(Vector3.back, Quaternion.Euler(-90f, 0f, 0f));
         // S2. bind them to the parent
         childA.transform.SetParent(transform, false);
         childB.transform.SetParent(transform, false);
+        childC.transform.SetParent(transform, false);
+        childD.transform.SetParent(transform, false);
+        childE.transform.SetParent(transform, false);
         // Note:
         // We don't bind to parent immediately, because that will change the hiarchy of the parent,
         // and by the time of creating the 2nd child, it will take both parent and its 1st child as the reference,
@@ -24,11 +30,12 @@ public class Fractal : MonoBehaviour
         // Solution: create all children, then bind them to parent one-by-one.
     }
 
-    Fractal CreateChild(Vector3 direction)
+    Fractal CreateChild(Vector3 direction, Quaternion rotation)
     {
         Fractal child = Instantiate(this);
         child.depth = depth - 1;
         child.transform.localPosition = 0.75f * direction;
+        child.transform.localRotation = rotation;
         child.transform.localScale = 0.5f * Vector3.one;
         return child;
     }
