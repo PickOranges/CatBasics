7669760bfc555adb95b1c59a026a4d3442eb613c
chengzimdl@gmail.com <chengzimdl@gmail.com>
2024-02-07 17:00:54 +0100

    Let each thread to deal with 5 children, but there's no big difference.

--- Assets/Scripts/T6/FractalJob.cs
+++ Assets/Scripts/T6/FractalJob.cs
@@ -146,7 +146,7 @@ public class FractalJob : MonoBehaviour
                 parents = parts[li - 1],
                 parts = parts[li],
                 matrices = matrices[li]
-            }.ScheduleParallel(parts[li].Length, 1, jobHandle);
+            }.ScheduleParallel(parts[li].Length, 5, jobHandle);
         }
         jobHandle.Complete();
 
